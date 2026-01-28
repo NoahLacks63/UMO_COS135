4df64df6cc9209978b18d6cf53e62d76371c6340
NoahLacks63 <noahlacks63@gmail.com>
2026-01-27 23:28:47 -0500

    Init commit

---
+++ HW1/Printing.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	//Hello UMaine!
+	printf("Hello,\nUMaine!\n\n");
+
+	//Right Asterisk Triangle
+	printf("    *\n   **\n  ***\n ****\n*****\n\n");
+	
+	//Upside Down Asterisk Triangle
+	printf("*****\n ***\n  *\n\n");
+	
+	//Left Triangle
+	printf("|\\\n| \\\n|  \\\n|___\\\n\n");
+
+	//Big Dipper
+	printf("  **  *\n *  **\n*\n\n");
+	
+	return 0;
+
+	//I forgot to put a return statement
+}
+
